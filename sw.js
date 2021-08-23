59a9e36472e2c68c50374e86d3ff7eddddb526af
superwibr <superwibr@gmail.com>
2021-08-23 10:09:37 -0400

    format

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //remember to increment the version # when you update the service worker
 const version = "1.03",
     preCache = "PRECACHE-" + version,
-    cacheList = [ "/" ];
+    cacheList = ["/"];
 
 /*
 create a list (array) of urls to pre-cache for your application
@@ -9,29 +9,29 @@ create a list (array) of urls to pre-cache for your application
 
 /*  Service Worker Event Handlers */
 
-self.addEventListener( "install", function ( event ) {
-    console.log( "Installing the service worker!" );
+self.addEventListener("install", function (event) {
+    console.log("Installing the service worker!");
     self.skipWaiting();
-    caches.open( preCache )
-        .then( cache => {
-            cache.addAll( cacheList );
-        } );
-} );
+    caches.open(preCache)
+        .then(cache => {
+            cache.addAll(cacheList);
+        });
+});
 
-self.addEventListener( "activate", function ( event ) {
+self.addEventListener("activate", function (event) {
     event.waitUntil(
         //wholesale purge of previous version caches
-        caches.keys().then( cacheNames => {
-            cacheNames.forEach( value => {
-                if ( value.indexOf( version ) < 0 ) {
-                    caches.delete( value );
+        caches.keys().then(cacheNames => {
+            cacheNames.forEach(value => {
+                if (value.indexOf(version) < 0) {
+                    caches.delete(value);
                 }
-            } );
-            console.log( "service worker activated" );
+            });
+            console.log("service worker activated");
             return;
-        } )
+        })
     );
-} );
+});
 
 
 /* service worker resources
